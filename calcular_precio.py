57155633bfc7fca59c41f2d1f1eb88571762c0d8
LAB_C_REAL <labc@continental.edu.pe>
2026-07-06 10:45:46 -0500

    Cambio precio de soles a dolares

--- calcular_precio.py
+++ calcular_precio.py
@@ -3,10 +3,10 @@ print(" ===== PRECIO FINAL =====")
 print(" AUTOR: PROGRAMADORES CONTI ")
 print()
 #lectura de datos
-PRECIO=float(input("Ingrese precio de venta, en soles: "))
+PRECIO=float(input("Ingrese precio de venta, en dolares: "))
 print()
 #proceso
 if PRECIO>200:
     PF=round(PRECIO*0.8,1)
-    print(f"El monto a pagar es: {PF} soles\n")
+    print(f"El monto a pagar es: {PF} dolares\n")
 #fin
