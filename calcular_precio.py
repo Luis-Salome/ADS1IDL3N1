c8effa5361ecacf9ec49d327a776231aab0271d2
LAB_C_REAL <labc@continental.edu.pe>
2026-07-06 10:56:59 -0500

    Se aumentó IGV 18% al precio final

--- calcular_precio.py
+++ calcular_precio.py
@@ -7,6 +7,6 @@ PRECIO=float(input("Ingrese precio de venta, en dolares: "))
 print()
 #proceso
 if PRECIO>300:
-    PF=round(PRECIO*0.9,1)
-    print(f"El monto a pagar es: {PF} dolares\n")
+    PF=round((PRECIO*0.9)*1.18,1)
+    print(f"El monto a pagar con IGV es: {PF} dolares\n")
 #fin
