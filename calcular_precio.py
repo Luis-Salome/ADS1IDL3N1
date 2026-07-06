16a96a1e42b188113377d0d1ae46252dc33e7984
LAB_C_REAL <labc@continental.edu.pe>
2026-07-06 10:48:29 -0500

    Precio mayor a 300 tiene descuento

--- calcular_precio.py
+++ calcular_precio.py
@@ -6,7 +6,7 @@ print()
 PRECIO=float(input("Ingrese precio de venta, en dolares: "))
 print()
 #proceso
-if PRECIO>200:
+if PRECIO>300:
     PF=round(PRECIO*0.8,1)
     print(f"El monto a pagar es: {PF} dolares\n")
 #fin
